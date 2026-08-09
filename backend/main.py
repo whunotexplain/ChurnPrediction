a6f3c67e45c0773776e73683f281f5eb77e9cfb9
whunotexplain <kozinegor2906@gmail.com>
2026-08-09 12:02:02 +0300

    code end to deploy ig

--- backend/main.py
+++ backend/main.py
@@ -68,3 +68,9 @@ def root():
         "health": "/api/v1/health",
         "version": settings.VERSION,
     }
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
